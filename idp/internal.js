0fab04613c468c2ab173600d7f5fcb9ab5a5a307
mmussett <mmussett@tibco.com>
2018-08-06 17:38:42 +0100

    Added Auth Proxy call

--- idp/internal.js
+++ idp/internal.js
@@ -71,6 +71,55 @@ function sambaApiCall(args, successCallback, errorCallback) {
 
 }
 
+function authApiCall(args, successCallback, errorCallback) {
+
+    var client = new restClient();
+
+    var opts = {
+        data: { username: args.username, password: args.password},
+        headers: {
+            "Content-Type": "application/json",
+        }
+    };
+
+    var req = client.post("http://35.159.8.83:8090/authenticate", opts, function(data, response) {
+        console.log("\n##### Response from Auth Proxy API #####");
+        console.log("Auth Proxy Response Status Code: " + response.statusCode)
+        console.log("Auth Proxy Data: "+ data);
+
+        successCallback(data);
+    });
+
+
+    req.on('requestTimeout', function (req) {
+        console.log('request has expired');
+        req.abort();
+        errorCallback(Error('Request Timeout'));
+    });
+
+    req.on('responseTimeout', function (res) {
+        console.log('response has expired');
+        errorCallback(Error('Response Timeout'));
+
+    });
+
+    req.on('error', function (err) {
+        console.log('request error', err);
+        errorCallback(err);
+    });
+
+
+}
+
+function authApiCallWrapper(args) {
+    return new Promise((resolve, reject) => {
+        authApiCall(args, (successResponse) => { resolve(successResponse);
+        }, (errorResponse) => {
+            reject(errorResponse)
+        });
+    });
+}
+
 function sambaApiCallWrapper(args) {
     return new Promise((resolve, reject) => {
         sambaApiCall(args, (successResponse) => { resolve(successResponse);
@@ -89,7 +138,7 @@ function desmonApiCallWrapper(args) {
     });
 }
 
-async function authenticate(username, password) {
+async function authenticateOld(username, password) {
 
     var result = false;
 
@@ -120,6 +169,36 @@ async function authenticate(username, password) {
     return result;
 
 }
+
+async function authenticate(username, password) {
+
+    var result = false;
+
+    try {
+
+        var authArgs = {
+            username: username,
+            password: password
+        };
+        const authResult = await authApiCallWrapper(authArgs);
+
+        console.log(authResult)
+        if (authResult == "OK") {
+            result = true;
+        } else {
+            result = false;
+        }
+        console.log('Auth Proxy returned: ' + sambaResult);
+
+    } catch(error) {
+        console.log(error);
+        return result;
+    }
+
+    return result;
+
+}
+
 
 
 exports.verifyCredentials = function(username, password, callback) {
